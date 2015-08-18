0f63f746488e5d4d98379786553c7d6ceedb9867
Remo Lüönd <remo.zumsteg@outlook.com>
2015-08-19 00:42:04 +0200

    implemented auth state handlers and bugfixing

--- example.js
+++ example.js
@@ -1,28 +1,14 @@
-var authState;
+var path = require('path');
 
-var auth = require('node-onedrive-auth').Provider({
-	clientId: '',
-	clientSeceret: '',
-	scope: [],
-	flow: 'code'
+var auth = require('./lib/index').AuthProvider({
+	clientId: '0000000048150D2D',
+	clientSecret: 'hG9BZpcd3tkiHCmfDp-HvvAfJa2yaRFj',
+	scope: ['wl.signin', 'wl.offline_access', 'onedrive.readwrite'],
+	flow: 'code',
 	port: 52763,
-	authState: authState
-});
-
-auth.on('statechanged', function(state){
-	authState = state;
+	authState: path.join(__dirname, 'auth-state.json')
 });
 
 auth.token().then(function(token){
-	
-});
-
-auth.api({
-	url: '',
-	method: '',
-	form: {}
-}).done(function(call){
-
-}, function(call){
-
-});
+	console.log(token);
+}).done();

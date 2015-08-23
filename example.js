de2af65434a98e8c15dcc68b50de0785093a42f0
Remo Lüönd <remo.zumsteg@outlook.com>
2015-08-23 23:53:33 +0200

    bugfixing

--- example.js
+++ example.js
@@ -1,14 +1,18 @@
 var path = require('path');
 
 var auth = require('./lib/index').AuthProvider({
-	clientId: 'client_id',
-	clientSecret: 'client_secret',
+	clientId: '0000000048150D2D',
+	clientSecret: '3B-vpMs6uabDFfDAh2hiPD3jC4tjoL4z',
 	scope: ['wl.signin', 'wl.offline_access', 'onedrive.readwrite'],
 	flow: 'code',
 	port: 52763,
 	authState: path.join(__dirname, 'auth-state.json')
 });
 
-auth.token().then(function(token){
-	console.log(token);
-}).done();
+auth.api({
+	uri: 'https://api.onedrive.com/v1.0/drive/root:/Music'
+}).done(function (result) {
+	console.log(result.body);
+}, function (result) {
+	throw result.err;
+});
